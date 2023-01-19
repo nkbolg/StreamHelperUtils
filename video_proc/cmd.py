7da0f6cf3fc4bcbd460b4acefea496ffc104c43d
nkbolg <nkbolg@gmail.com>
2023-01-19 10:50:13 +0300

    splited generated thing to folders, added mp3 gen

--- video_proc/cmd.py
+++ video_proc/cmd.py
@@ -9,7 +9,7 @@ cmd1_template = """ffmpeg -i "{input_file}" -i short.png -filter_complex "
        [temp][1:v] overlay=x=0:y=0:enable='between(t,0,1)' [temp]; \
        [0:v] setpts=PTS+1/TB, scale=1920x1080:force_original_aspect_ratio=decrease, pad=1920:1080:-1:-1:color=black [v:0]; \
        [temp][v:0] overlay=x=0:y=0:shortest=1:enable='gt(t,1)' [v]; \
-       [0:a] asetpts=PTS+1/TB [a]" -map [v] -map [a] "{out_file}.mp4" """
+       [0:a] asetpts=PTS+1/TB [a]" -map [v] -map [a] "{out_file}" """
 
 cut_piece_template = "ffmpeg -ss {start_time} -to {end_time} -i \"{input_file}\" -c copy \"{out_file}\""
 
@@ -21,17 +21,25 @@ os.chdir(dir_name)
 
 @dataclass
 class Part:
-    title: str
+    author: str
+    song_name: str
     start_time: str
     end_time: str
+
+    @property
+    def title(self):
+        return f"{self.author} — {self.song_name}"
 
 
 def read_parts(filename: str) -> list[Part]:
     xs = []
     with open(filename) as f:
         for line in f:
-            start_time, _, end_time, *name = line.split(" ")
-            xs.append(Part(title=' '.join(name).strip(), start_time=start_time, end_time=end_time))
+            start_time, _, end_time, *name = line.rstrip().split(" ")
+            pos = name.index('-')
+            author = ' '.join(name[:pos])
+            song_name = ' '.join(name[pos+1:])
+            xs.append(Part(author=author, song_name=song_name, start_time=start_time, end_time=end_time))
     return xs
 
 
@@ -48,22 +56,39 @@ def fmt_time(time: str) -> str:
         raise RuntimeError("Unexpected time str: " + time)
 
 
+if not os.path.exists('res'):
+    os.mkdir('res')
+
+if not os.path.exists('piece'):
+    os.mkdir('piece')
+
+if not os.path.exists('mp3'):
+    os.mkdir('mp3')
+
 for part in parts:
     print(f"Processing {part.title}")
 
-    piece_filename = part.title + '.mp4'
+    piece_filename = os.path.join('piece',  part.title + '.mp4')
 
-    cut_cmd = cut_piece_template.format(start_time=fmt_time(part.start_time),
-                                        end_time=fmt_time(part.end_time),
-                                        input_file=in_file,
-                                        out_file=piece_filename)
+    if not os.path.exists(piece_filename):
+        cut_cmd = cut_piece_template.format(start_time=fmt_time(part.start_time),
+                                            end_time=fmt_time(part.end_time),
+                                            input_file=in_file,
+                                            out_file=piece_filename)
+        subprocess.run(cut_cmd, shell=True, check=True)
 
-    subprocess.run(cut_cmd, shell=True, check=True)
+    res_file = os.path.join('res', f"{part.title}.mp4")
 
-    image_cmd = image_template.format(part.title)
+    if not os.path.exists(res_file):
+        image_cmd = image_template.format(f"{part.author}\n{part.song_name}")
+        subprocess.run(image_cmd, shell=True, check=True)
 
-    subprocess.run(image_cmd, shell=True, check=True)
+        cmd1 = cmd1_template.format(input_file=piece_filename, out_file=res_file)
+        subprocess.run(cmd1, shell=True, check=True)
 
-    cmd1 = cmd1_template.format(input_file=piece_filename, out_file=part.title + '_out')
+    music_file = os.path.join('mp3', f"{part.title}.mp3")
 
-    subprocess.run(cmd1, shell=True, check=True)
+    if not os.path.exists(music_file):
+        subprocess.run(f'ffmpeg -i "{piece_filename}" -metadata title="{part.song_name}" -metadata artist="{part.author}" "{music_file}"', shell=True, check=True)
+
+
